9ab721d6cb37bdfd0838c7d2854e8280a45cd0ef
Bhavesh Adhikari <bhaveshadhikari71@gmail.com>
2026-01-18 00:14:05 +0545

    convert B.S to A.D minimum working example

---
+++ bs_to_ad.py
@@ -0,0 +1,23 @@
+from data import TOTAL_BS_MONTHLY_DAYS, MONTH_START_WEEKDAY
+from data import NEW_YEAR_BS_AD_MAP
+from datetime import date, timedelta
+
+bs_date = "2080-9-30"
+year,month,day = bs_date.split("-")
+year, month, day = (int)(year), (int)(month), (int)(day)
+year_start_ad_date = NEW_YEAR_BS_AD_MAP[f'{year}']
+ad_year, ad_month, ad_day = year_start_ad_date.split('-')
+total_monthly_days_for_year = TOTAL_BS_MONTHLY_DAYS[year-2000]
+# print(total_monthly_days_for_year)
+month_idx = month - 1
+day_idx = day - 1
+if month_idx > 0 :
+    days_offset_from_year_start = sum(total_monthly_days_for_year[0:month_idx])+day_idx
+else:
+    days_offset_from_year_start = day_idx
+    
+# print(days_offset_from_year_start)
+
+
+d = date(int(ad_year), int(ad_month), int(ad_day)) + timedelta(days=days_offset_from_year_start)
+print(d)
