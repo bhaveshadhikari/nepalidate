6ec1ffdffeec0eae4d039d8dada5a10c79d6a195
Bhavesh Adhikari <bhaveshadhikari71@gmail.com>
2026-01-18 13:56:36 +0545

    add current_datetime function to retrieve and display AD and BS dates with time and weekday

---
+++ current_datetime.py
@@ -0,0 +1,34 @@
+from ad_to_bs import ad_to_bs
+from datetime import datetime
+import pytz
+
+class DateTimeInfo:
+    def __init__(self, ad_date, bs_date, time, weekday):
+        self.ad_date = ad_date
+        self.bs_date = bs_date
+        self.time = time
+        self.weekday = weekday
+
+def current_datetime():
+    nepal_tz = pytz.timezone('Asia/Kathmandu')
+    ad_datetime_now = datetime.now(nepal_tz)
+    ad_date = ad_datetime_now.strftime("%Y-%m-%d")
+    time = ad_datetime_now.strftime("%H:%M:%S")
+    weekday = ad_datetime_now.strftime("%A")
+    bs_date = ad_to_bs(ad_date)
+
+    return DateTimeInfo(
+        ad_date,
+        bs_date,
+        time,
+        bs_date
+    )
+
+if __name__ == '__main__':
+    dt=current_datetime()
+    print(dt.ad_date)
+    print(dt.bs_date)
+    print(dt.time)
+    print(dt.weekday)
+
+    
