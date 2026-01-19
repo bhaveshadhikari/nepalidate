f556159aaebd20432a77404006f848baa4cc114f
Bhavesh Adhikari <bhaveshadhikari71@gmail.com>
2026-01-19 12:59:25 +0545

    updated allow origin

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from datetime import datetime
-import pytz
+from fastapi.middleware.cors import CORSMiddleware
 from utils.current_datetime import datetime_now
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://localhost:5173"],  # React dev servers
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/widget/clock", response_class=HTMLResponse)
-
 def clock_widget(request: Request):
-    
     dt = datetime_now()
-
+    
     return templates.TemplateResponse(
         "clock_widget.html",
         {
